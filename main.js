5f401f5f059224246172c30ebc526a73d24c4971
Dávid Jopčík <david.jopcik@gmail.com>
2022-10-05 20:29:51 +0200

    Copy my code

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
             selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            // solution
+            sizes: ['S', 'M', 'L', 'XL'],
+            // solution
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ],
-            // solution
-            onSale: true
-            // solution
+              { id: 2234, color: 'blue', image:'./assets/images/socks_blue.jpg', quantity: 50 },
+              { id: 2235, color: 'green', image:'./assets/images/socks_green.jpg', quantity: 0},
+              { id: 2235, color: 'green', image:'./assets/images/socks_green.jpg', quantity: 1},
+            ]
+            
         }
     },
-    methods: {
-        addToCart() {
+    methods:{
+        addToCart(){
             this.cart += 1
         },
-        updateVariant(index) {
+        removeFromCart(){
+            if(this.cart > 0){
+                this.cart -= 1
+            }
+        }, 
+        updateVariant(index){
             this.selectedVariant = index
+            console.log(this.inStock);
         }
     },
     computed: {
-        title() {
+        title(){
             return this.brand + ' ' + this.product
         },
-        image() {
+        image(){
             return this.variants[this.selectedVariant].image
         },
-        inStock() {
+        inStock(){
             return this.variants[this.selectedVariant].quantity
         },
-        // solution
-        sale() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' is on sale.'
-            }
-            return ''
-        }
-        // solution
+
     }
+
 })
